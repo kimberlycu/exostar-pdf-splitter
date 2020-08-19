5a621e79f10b039b92829d37f0bec2f12265a895
Kim Cu <44420785+kimberlycu@users.noreply.github.com>
2020-08-19 15:32:46 -0700

    Update pdfSplitter.py

--- pdfSplitter.py
+++ pdfSplitter.py
@@ -1,44 +1,51 @@
 # Author: Kimberly Cu
-# Date: 8-18-2020
+# Date: 8-19-2020
 # Project: Exostar PDF Splitter
 # Description: Python script to split batch purchase order downloads from Exostar into separate PDF files
 # with the order number and revision number as the file name.
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-# Converts a string of chars and numbers into a string with only numbers. Returns string.
+# Converts a string of numbers and chars into a string with only numbers. Returns string.
 def turnToNum(x):
   numeric_filter = filter(str.isdigit, x)
   numeric_string = "".join(numeric_filter)
   return numeric_string
 
+# Extracts order and revision number from given page number. Returns order and revision number.
+def orderRevNum(readerObj, currPageNum):
+  pageObj = readerObj.getPage(currPageNum)
+  text = pageObj.extractText().split(" ")
+  orderNum = turnToNum(text[0])
+  revNum = turnToNum(text[4])
+  return orderNum, revNum
+
+# Writes pages stored from PdfFileWriter into a PDF file with order and revision number as the file name.
+# Closes the file stream.
+def writeToFile(oNum, rNum, writerObj):
+    output = oNum + " POC" + rNum + ".pdf"
+    outputStream = open(output, "wb")
+    writerObj.write(outputStream)
+    outputStream.close()
+
 # Initialize a reader and writer object.
-inputStream = open("test2.pdf", "rb")
+inputStream = open("test3.pdf", "rb")
 reader = PdfFileReader(inputStream)
 writer = PdfFileWriter()
 
-# Parse first page of the PDF with space delimiter. Extract order number and revision number.
-pageObj = reader.getPage(0)
-text = pageObj.extractText().split(" ")
-orderNum = turnToNum(text[0])
-revNum = turnToNum(text[4])
+# Get order number and revision number from first page of PDF.
+orderNum, revNum = orderRevNum(reader, 0)
 
-# For each page in PDF, extract order and revision number.
+# For each page in PDF, get order and revision number.
 for pageNum in range(reader.getNumPages()):
-  currPageObj = reader.getPage(pageNum)
-  currText = currPageObj.extractText().split(" ")
-  currOrderNum = turnToNum(currText[0])
-  currRevNum = turnToNum(currText[4])
+  currOrderNum, currRevNum = orderRevNum(reader, pageNum)
 
 # If order number of the last page does not match the current order number, write pages from writer object to
 # a PDF with the order and revision number as the file name. Close output file stream. Reset writer object to get rid 
 # of previously passed in pages. Set the current order and revision number as the older order and revision number
 # for comparison.
   if orderNum != currOrderNum:
-    output = orderNum + " POC" + revNum + ".pdf"
-    outputStream = open(output, "wb")
-    writer.write(outputStream)
-    outputStream.close()
+    writeToFile(orderNum, revNum, writer)
     writer = PdfFileWriter()
     orderNum = currOrderNum
     revNum = currRevNum
@@ -49,10 +56,7 @@ for pageNum in range(reader.getNumPages()):
 # If there are no more pages, write pages from writer object to new PDF with the order and revision number as the
 # file name. Close output file stream.
   if pageNum == reader.getNumPages()-1:
-    output = orderNum + " POC" + revNum + ".pdf"
-    outputStream = open(output, "wb")
-    writer.write(outputStream)
-    outputStream.close()
+    writeToFile(orderNum, revNum, writer)
 
 # Close input file stream.
 inputStream.close()
